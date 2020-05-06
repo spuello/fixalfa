eb76b6a6cbf5e4159e1a26484f4e034e127ff65f
Samuel Puello <spuellocoronado@gmail.com>
2020-05-06 15:05:42 -0400

    add user repository

---
+++ lib/repositories/user_repositories.dart
@@ -0,0 +1,47 @@
+import 'package:app/data_layer/user.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class UserRepository {
+  FirebaseAuth _firebaseAuth;
+  GoogleSignIn _googleSignIn;
+
+  UserRepository({FirebaseAuth firebaseAuth, GoogleSignIn googleSignIn})
+      : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance,
+        _googleSignIn = googleSignIn ?? GoogleSignIn();
+
+  Future<FirebaseUser> signInWithGoogle() async {
+    GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+
+    GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+
+    final AuthCredential credential = GoogleAuthProvider.getCredential(
+        idToken: googleAuth.idToken, accessToken: googleAuth.accessToken);
+
+    await _firebaseAuth.signInWithCredential(credential);
+    return _firebaseAuth.currentUser();
+  }
+
+  Future<void> signInWithCredential(String email, String password) async {
+    return await _firebaseAuth.signInWithEmailAndPassword(
+        email: email, password: password);
+  }
+
+  Future<void> signUp({String email, String password}) async {
+    await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email, password: password);
+  }
+
+  Future<void> signOut() async {
+    return Future.wait([_firebaseAuth.signOut(), _googleSignIn.signOut()]);
+  }
+
+  Future<bool> isSignedIn() async {
+    return await _firebaseAuth.currentUser() != null;
+  }
+
+  Future<User> getUser() async {
+    var currentUser = await _firebaseAuth.currentUser();
+    return User(email: currentUser.email);
+  }
+}
